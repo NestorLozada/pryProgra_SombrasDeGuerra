5a8ea40cd3c252f34ae409b5fdcc5fdaa69dcbe2
NestorLozada <martinlozada1@gmail.com>
2022-05-16 11:51:25 -0500

    Add Doors, Add openDoors

---
+++ Assets/Scripts/SystemDoor.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemDoor : MonoBehaviour
+{
+    
+    public bool doorOpen = false;
+    public float doorOpenAngel= 95f; 
+    public float doorCloseAngle = 0.0f ; 
+    public float smooth = 3.0f; 
+
+    public void ChangeDoorState()
+    {
+        doorOpen = !doorOpen; 
+    }
+    
+    
+    void Update()
+    {
+        if (doorOpen)
+        {
+            Quaternion targetRotation = Quaternion.Euler(0,doorOpenAngel,0);
+            transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRotation, smooth*Time.deltaTime);
+
+        }
+        else 
+        {
+            Quaternion targetRotation2 = Quaternion.Euler(0,doorCloseAngle,0);
+            transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRotation2, smooth*Time.deltaTime);
+        }
+    }
+}
