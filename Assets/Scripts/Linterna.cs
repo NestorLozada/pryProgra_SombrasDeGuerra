fe1819ae832aeed7db4da458b8834f848b2f4ae9
NestorLozada <martinlozada1@gmail.com>
2022-05-16 16:08:57 -0500

    add interactive flashligh

---
+++ Assets/Scripts/Linterna.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linterna : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Flashligh_Light; 
+    private bool isOn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Flashligh_Light.gameObject.SetActive ( false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if(isOn ==false)
+            {
+                Flashligh_Light.gameObject.SetActive(true);
+                isOn = true;
+            }
+            else
+            {
+                Flashligh_Light.gameObject.SetActive(false);
+                isOn = false;
+            }
+        }    
+    }
+}
