684c68f2241e2ae243098ffc030487cf4c008dd6
NestorLozada <martinlozada1@gmail.com>
2022-10-06 20:42:10 -0500

    cosas

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class Movimiento : MonoBehaviour
 {
@@ -8,11 +10,7 @@ public class Movimiento : MonoBehaviour
     public CharacterController controller;
     public float speed = 5f;
     public float gravity = -9.8f;
-    public float jumpHeight = 3;
-
-    public Transform groundCheck;
-    public float groundDistance = 0.3f;
-    public LayerMask groundMask;
+ 
 
     Vector3 velocity;
     bool isGrounded;
@@ -25,22 +23,13 @@ public class Movimiento : MonoBehaviour
     void Update()
     {
 
-        isGrounded = Physics.CheckSphere(groundCheck.position,groundDistance,groundMask);
-        if(isGrounded && velocity.y<0)
-        {
-            velocity.y = -2f;
-        }
+        //OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller);
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move*speed*Time.deltaTime);
-
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2*gravity);
-        }
 
         velocity.y += gravity * Time.deltaTime;
 
